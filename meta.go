b8b61a09433eee9c23c8e5e371cbf15d9e6fd7cf
Rafael Lefever <rafael.lefever@discomco.pl>
2022-05-02 10:32:31 +0200

    initial commit

---
+++ meta.go
@@ -0,0 +1,15 @@
+package go_dec
+
+type Meta struct {
+	ID      Identity
+	TraceId string
+	Status  int
+}
+
+func NewMeta(id Identity, traceId string, status int) *Meta {
+	return &Meta{
+		ID:      id,
+		TraceId: traceId,
+		Status:  status,
+	}
+}
